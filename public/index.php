a56f9d1748e64105da2c5bebeb597ce40416b8f7
Gaurang Kumar <gaurangkumarp@gmail.com>
2018-11-29 22:35:54 +0530

    built-in server code error fixed

--- public/index.php
+++ public/index.php
@@ -16,8 +16,7 @@ require dirname(__DIR__) . '/config/requirements.php';
 // For built-in server
 if (PHP_SAPI === 'cli-server') {
     $_SERVER['PHP_SELF'] = '/' . basename(__FILE__);
-
-    $url = parse_url(urldecode($_SERVER['REQUEST_URI']));
+    $url = parse_url(urldecode('/'.ltrim($_SERVER['REQUEST_URI'], '/')));
     $file = __DIR__ . $url['path'];
     if (strpos($url['path'], '..') === false && strpos($url['path'], '.') !== false && is_file($file)) {
         return false;
