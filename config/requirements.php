d0d48918d7e7dc74d28f972da98130948f4a7b86
Gaurang Kumar <gaurangkumarp@gmail.com>
2018-11-29 20:36:20 +0530

    copyright removed

--- config/requirements.php
+++ config/requirements.php
@@ -1,19 +1,6 @@
 <?php
 /**
- * The Front Controller for handling every request
- *
- * IronPHP : PHP Development Framework
- * Copyright (c) IronPHP (https://github.com/IronPHP/IronPHP)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @package       IronPHP
- * @copyright     Copyright (c) IronPHP (https://github.com/IronPHP/IronPHP)
- * @link          
- * @since         0.0.1
- * @license       MIT License (https://opensource.org/licenses/mit-license.php)
+ * @package       IronPHP Application
  * @auther        GaurangKumar Parmar <gaurangkumarp@gmail.com>
  */
 
