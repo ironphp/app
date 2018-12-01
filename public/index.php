e441081b3f7069976cf34a066d442b091185430e
Gaurang Kumar <gaurangkumarp@gmail.com>
2018-12-01 16:35:29 +0530

    cli-server improved

--- public/index.php
+++ public/index.php
@@ -15,10 +15,16 @@ require dirname(__DIR__) . '/config/requirements.php';
 
 // For built-in server
 if (PHP_SAPI === 'cli-server') {
-    $_SERVER['PHP_SELF'] = '/' . basename(__FILE__);
-    $url = parse_url(urldecode('/'.ltrim($_SERVER['REQUEST_URI'], '/')));
-    $file = __DIR__ . $url['path'];
-    if (strpos($url['path'], '..') === false && strpos($url['path'], '.') !== false && is_file($file)) {
+	$_SERVER['PHP_SELF'] = '/' . basename(__FILE__);
+	$uri = urldecode(
+    	'/'.ltrim(parse_url($_SERVER['REQUEST_URI'], '/'), PHP_URL_PATH)
+	);
+	$file = __DIR__ . $uri['path'];
+	/*
+	 * This allows us to emulate Apache's "mod_rewrite" functionality from the
+     * built-in PHP web server.
+	 */
+    if (strpos($url['path'], '..') === false && strpos($url['path'], '.') !== false && is_file($file) && file_exists($file)) {
         return false;
     }
 }
