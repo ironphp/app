b316ffaf1802013db6882f6c545c5e4e60e60e16
Gaurang Kumar <gaurangkumarp@gmail.com>
2019-06-18 23:17:06 +0530

    Timing error fixed

--- public/index.php
+++ public/index.php
@@ -66,9 +66,9 @@ define('IRONPHP_END', microtime(true));
 if(env('APP_DEBUG')) {
     echo '<hr><pre>';
     echo 'CONFIG_LOADED   : '.round((CONFIG_LOADED - IRONPHP_START), 4)." seconds<br>";
-    echo 'REQUEST_CATCHED : '.round((REQUEST_CATCHED - CONFIG_LOADED), 4)." seconds<br>";
-    echo 'ROUTES_LOADED   : '.round((ROUTES_LOADED - REQUEST_CATCHED), 4)." seconds<br>";
-    echo 'ROUTE_MATCHED   : '.round((ROUTE_MATCHED - ROUTES_LOADED), 4)." seconds<br>";
+    echo 'ROUTES_LOADED   : '.round((ROUTES_LOADED - CONFIG_LOADED), 4)." seconds<br>";
+    echo 'REQUEST_CATCHED : '.round((REQUEST_CATCHED - ROUTES_LOADED), 4)." seconds<br>";
+    echo 'ROUTE_MATCHED   : '.round((ROUTE_MATCHED - REQUEST_CATCHED), 4)." seconds<br>";
     echo 'DISPATCHER_INIT : '.round((DISPATCHER_INIT - ROUTE_MATCHED), 4)." seconds<br>";
     echo 'DISPATCHED      : '.round((DISPATCHED - DISPATCHER_INIT), 4)." seconds<br>";
     echo 'RESPONSE_SEND   : '.round((RESPONSE_SEND - DISPATCHED), 4)." seconds<br>";
